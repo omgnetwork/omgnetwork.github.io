746cd3bdbc3ce78466d26a9bd1478a94c5819627
okalouti <omar@omisego.co>
2020-02-24 13:44:46 +0700

    [FORMAT]: back to single quote

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -3,49 +3,45 @@
 const siteConfig = {
   docsSideNavCollapsible: true,
   disableHeaderTitle: true,
-  gaTrackingId: "GTM-TVZS27N",
-  title: "OMG Network",
-  tagline: "Documentation for the OMG Network",
-  url: "https://nicholasmueller.github.io",
-  baseUrl: "/",
-  docsUrl: "",
-  projectName: "nicholasmueller.github.io",
-  organizationName: "nicholasmueller",
+  gaTrackingId: 'GTM-TVZS27N',
+  title: 'OMG Network',
+  tagline: 'Documentation for the OMG Network',
+  url: 'https://nicholasmueller.github.io',
+  baseUrl: '/',
+  docsUrl: '',
+  projectName: 'nicholasmueller.github.io',
+  organizationName: 'nicholasmueller',
   headerLinks: [
-    { doc: "welcome", label: "Docs" },
-    {
-      href: "network-connection-details",
-      external: false,
-      label: "Network Connection"
-    },
-    { doc: "api-reference-introduction", label: "API" },
-    { href: "https://omisego.co/", external: true, label: "Product" }
+    { doc: 'welcome', label: 'Docs' },
+    { href: 'network-connection-details', external: false, label: 'Network Connection' },
+    { doc: 'api-reference-introduction', label: 'API' },
+    { href: 'https://omisego.co/', external: true, label: 'Product' },
   ],
-  headerIcon: "img/omisego-white.svg",
-  footerIcon: "img/favicon.ico",
-  favicon: "img/favicon.ico",
+  headerIcon: 'img/omisego-white.svg',
+  footerIcon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
   colors: {
-    primaryColor: "#215cec",
-    secondaryColor: "#2e374c"
+    primaryColor: '#215cec',
+    secondaryColor: '#2e374c',
   },
   scripts: [],
-  onPageNav: "separate",
+  onPageNav: 'separate',
   cleanUrl: true,
   algolia: {
-    apiKey: "af8e7b1e7bcb8af0f31e1c3aca2f8d16",
-    indexName: "omisego"
+    apiKey: 'af8e7b1e7bcb8af0f31e1c3aca2f8d16',
+    indexName: 'omisego'
   },
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "default"
+    theme: 'default',
   },
   // Open Graph and Twitter card images.
-  ogImage: "img/omisego-blue.svg",
-  twitterImage: "img/omisego-blue.svg",
+  ogImage: 'img/omisego-blue.svg',
+  twitterImage: 'img/omisego-blue.svg',
 
   // Show documentation's last contributor's name.
   enableUpdateBy: false,
-  enableUpdateTime: false
+  enableUpdateTime: false,
 };
 
 module.exports = siteConfig;
