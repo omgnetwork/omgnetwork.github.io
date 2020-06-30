bab37321b22aa9dd1f00346a9f1f8648bffa1521
dmitrydao <dmitrydaopro@gmail.com>
2020-06-30 13:02:50 +0300

    [Add]: g-tag

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,6 +28,7 @@ const siteConfig = {
   },
   scripts: [
     'js/typeform.js',
+    'js/g-tag.js',
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-block-buttons.js',
