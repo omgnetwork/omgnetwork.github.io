ecaefa3d2f4f31631754cfc792a0ebaa1f240d38
dmitrydao <dmitrydaopro@gmail.com>
2020-11-23 13:20:13 +0200

    [Update]: 404 page

--- website/pages/en/404.js
+++ website/pages/en/404.js
@@ -1,10 +1,14 @@
 const React = require("react");
 
-const NotFoundPage = () => (
-  <React.Fragment>
-    <meta httpEquiv="refresh" content="0; url = https://docs.omg.network" />
-    <p>You're being redirected to <a href="https://docs.omg.network">docs.omg.network</a></p>
-  </React.Fragment>
-)
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <body>
+        <meta httpEquiv="refresh" content="0; url = https://docs.omg.network" />
+        <p className="mx-auto my-5">You're being redirected to <a href="https://docs.omg.network">docs.omg.network</a></p>
+      </body>
+    )
+  }
+}
 
 module.exports = NotFoundPage;
