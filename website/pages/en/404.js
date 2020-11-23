529bb75793471599b7036fb932edb9277e20189d
dmitrydao <dmitrydaopro@gmail.com>
2020-11-23 13:06:24 +0200

    [Update]: 404

---
+++ website/pages/en/404.js
@@ -0,0 +1,10 @@
+const React = require("react");
+
+const NotFoundPage = () => (
+  <React.Fragment>
+    <meta httpEquiv="refresh" content="0; url = https://docs.omg.network" />
+    <p>You're being redirected to <a href="https://docs.omg.network">docs.omg.network</a></p>
+  </React.Fragment>
+)
+
+module.exports = NotFoundPage;
